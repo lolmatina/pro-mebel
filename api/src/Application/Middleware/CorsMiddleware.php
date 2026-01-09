fbedbe64bb6e436e5d2a83da2e5076d2031f87cd
lolmatina <almat.orazali02@gmail.com>
2026-01-09 19:51:26 +0500

    Cors fix

--- api/src/Application/Middleware/CorsMiddleware.php
+++ api/src/Application/Middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware implements MiddlewareInterface
         $response = $handler->handle($request);
         
         return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Origin', 'https://promeb.kz')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
             ->withHeader('Access-Control-Allow-Credentials', 'false');
